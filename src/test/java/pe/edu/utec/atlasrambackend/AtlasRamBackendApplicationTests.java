33c75ea40841f7a90f589be14bb4e6cabe258c0a
Alvaro Quispe <alvaro.quispe.c@utec.edu.pe>
2026-09-15 02:11:34 -0500

    chore: add Spring Boot project structure

---
+++ src/test/java/pe/edu/utec/atlasrambackend/AtlasRamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utec.atlasrambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlasRamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
