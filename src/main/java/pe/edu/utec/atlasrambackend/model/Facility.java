b8ec82ac5f600a09715c5939e78ee29257068c39
Alvaro Quispe <alvaro.quispe.c@utec.edu.pe>
2026-09-15 09:01:49 -0500

    feat: add Facility entity with district relation

---
+++ src/main/java/pe/edu/utec/atlasrambackend/model/Facility.java
@@ -0,0 +1,34 @@
+package pe.edu.utec.atlasrambackend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name="facility")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Facility {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false,unique = true,length = 20)
+    private String code;
+
+    @NotBlank
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "district_id", nullable = false)
+    private District district;
+
+}
