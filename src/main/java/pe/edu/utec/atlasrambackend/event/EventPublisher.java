92e8c006265f1be18f4557a88aef2f236ca5afdc
Adriano Cosme <adriano.cosme@utec.edu.pe>
2026-09-20 18:53:04 -0500

    feat: add event publisher, listeners and mail service

---
+++ src/main/java/pe/edu/utec/atlasrambackend/event/EventPublisher.java
@@ -0,0 +1,18 @@
+package pe.edu.utec.atlasrambackend.event;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventPublisher {
+
+    private final ApplicationEventPublisher publisher;
+
+    public EventPublisher(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public void publish(Object event) {
+        publisher.publishEvent(event);
+    }
+}
